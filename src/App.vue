ee88a71848ae04158d8d6cef7068fd6093cf5f80
ymarcon <yannick.marcon@epfl.ch>
2023-09-18 20:38:10 +0200

    feat: secret (or sort of)

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import { mdiInformation } from '@mdi/js'
 import { useI18n } from 'vue-i18n'
 import { useLocale } from 'vuetify'
 import { useCookies } from 'vue3-cookies'
+import { ref } from 'vue'
+
+const showHome = ref<boolean>(false)
+const secret = ref<string>()
 
 const { current } = useLocale()
 const { locale } = useI18n({ useScope: 'global' })
@@ -19,11 +23,17 @@ function onLocale(lang: string) {
 function getCurrentLocaleOrFallback() {
   return ['en', 'fr'].includes(current.value) ? current.value : 'en'
 }
+
+function onSecretChange() {
+  if (secret.value === 'urbs2023') {
+    showHome.value = true
+  }
+}
 </script>
 
 <template>
   <v-app>
-    <v-app-bar flat height="68">
+    <v-app-bar flat height="68" v-if="showHome">
       <v-app-bar-title>
         <div class="text-h5">URBTREES</div>
         <div class="text-subtitle-2">{{ $t('app_subtitle') }}</div>
@@ -54,11 +64,30 @@ function getCurrentLocaleOrFallback() {
         </a>
       </template>
     </v-app-bar>
-    <v-main>
-      <RouterView />
+    <v-main v-if="showHome">
+      <RouterView/>
+    </v-main>
+    <v-main v-else>
+      <div>
+        <v-row class="mt-16">
+          <v-col cols="4">
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="secret"
+              label="Type secret and press Enter"
+              hide-details="auto"
+              @change="onSecretChange"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+          </v-col>
+        </v-row>
+      </div>
     </v-main>
   </v-app>
 </template>
+
 
 <style lang="scss" scoped>
 .v-app-bar {
